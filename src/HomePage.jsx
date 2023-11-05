682b4f5656861064c1dcdc98da8e5a6abf4aa8ec
Dixon-Afreh <dixonafreh20@gmail.com>
2023-11-05 11:36:56 +0000

    A hompage for the crown clothing

---
+++ src/HomePage.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <div>
+        <div className='HomePage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>HATS</h1>
+                <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>JAKET</h1>
+                <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>MAP</h1>
+                <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>HATS</h1>
+                <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>HATS</h1>
+                <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+        </div>
+    )
+    
+}
+
+
+
