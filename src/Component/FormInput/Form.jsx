ba24a9a358e0d3709b147bc9421b754bed6b0e09
Dixon-Afreh <dixonafreh20@gmail.com>
2023-11-14 23:40:06 +0000

    form

---
+++ src/Component/FormInput/Form.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Form() {
+  return (
+    <div>
+          <div className="form">
+              <input type="button" value="name" />
+              <input type="checkbox" name="schoool" id="" />
+              <input type="checkbox" name="schoool" id="" />
+              <input type="checkbox" name="schoool" id="" />
+              <input type="checkbox" name="schoool" id="" />
+              <input type="checkbox" name="schoool" id="" />
+              <input type="checkbox" name="schoool" id="" />
+              <input type="checkbox" name="schoool" id="" />
+              <input type="checkbox" name="schoool" id="" />
+              <input type="checkbox" name="schoool" id="" />
+              <input type="checkbox" name="schoool" id="" />
+              
+      </div>
+    </div>
+  )
+}
